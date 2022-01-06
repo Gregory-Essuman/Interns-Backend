94722a8929bcbaa39aa3f9713930c936a8edcec4
Gregory-Essuman <gregory.essuman@gmail.com>
2022-01-06 00:43:07 +0000

    debug value

--- Data_Collection_API/settings.py
+++ Data_Collection_API/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = 'TRUE'
 
 ALLOWED_HOSTS = ['localhost', 'collectdata2021.herokuapp.com']
 
